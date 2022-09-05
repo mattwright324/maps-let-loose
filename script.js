ad85861479f7a68a261ee1f67d0090c192c4add8
Matthew Wright <mattwright324@gmail.com>
2022-09-05 19:19:23 -0400

    reduce unnecessary calls; tweaks

--- script.js
+++ script.js
@@ -167,6 +167,13 @@
                 repeat: 'repeat'
             }, controls.fabricCanvas.renderAll.bind(controls.fabricCanvas))
 
+            const eCanvas = document.createElement("canvas");
+            controls.exportCanvas = new fabric.Canvas(eCanvas, {
+                scale: 1,
+                width: 1920,
+                height: 1920
+            })
+
             elements.sectorA = new fabric.Rect({
                 zIndex: 4,
                 opacity: 0.20,
@@ -176,6 +183,7 @@
                 visible: false
             });
             controls.fabricCanvas.add(elements.sectorA);
+            controls.exportCanvas.add(elements.sectorA);
             elements.sectorB = new fabric.Rect({
                 zIndex: 4,
                 opacity: 0.20,
@@ -185,14 +193,18 @@
                 visible: false
             });
             controls.fabricCanvas.add(elements.sectorB);
+            controls.exportCanvas.add(elements.sectorB);
 
             fabric.Image.fromURL('', function (img) {
                 elements.map = img;
 
                 img.selectable = false;
                 img.zIndex = 0;
+
                 controls.fabricCanvas.add(img);
                 controls.fabricCanvas.orderByZindex();
+                controls.exportCanvas.add(img);
+                controls.exportCanvas.orderByZindex();
             });
             fabric.Image.fromURL('./maps/plain-grid.png', function (img) {
                 elements.grid = img;
@@ -201,6 +213,8 @@
                 img.zIndex = 1;
                 controls.fabricCanvas.add(img);
                 controls.fabricCanvas.orderByZindex();
+                controls.exportCanvas.add(img);
+                controls.exportCanvas.orderByZindex();
             });
             elements.strongpoints = [[], [], [], [], []]
             for (let x = 0; x < 5; x++) {
@@ -208,8 +222,11 @@
                     fabric.Image.fromURL('', function (img) {
                         img.selectable = false;
                         img.zIndex = 3;
+
                         controls.fabricCanvas.add(img);
                         controls.fabricCanvas.orderByZindex();
+                        controls.exportCanvas.add(img);
+                        controls.exportCanvas.orderByZindex();
 
                         elements.strongpoints[x].push(img);
                     });
@@ -222,6 +239,8 @@
                 img.visible = $("#dg-visible").is("checked");
                 controls.fabricCanvas.add(img);
                 controls.fabricCanvas.orderByZindex();
+                controls.exportCanvas.add(img);
+                controls.exportCanvas.orderByZindex();
             });
 
             internal.setupPage();
@@ -415,6 +434,13 @@
                 internal.updateStatesAndRender();
             })
 
+            controls.btnSave.click(function () {
+                $('<a>').attr({
+                    href: controls.exportCanvas.toDataURL(),
+                    download: controls.comboMapSelect.val() + "_Custom_MLL.png"
+                })[0].click();
+            });
+
             let lastRangeVal = controls.sectorRange.val();
             controls.sectorRange.on('input', function () {
                 if (controls.sectorRange.val() !== lastRangeVal) {
@@ -474,17 +500,17 @@
                 elements.sectorB.set({fill: '#08FFFF'});
             }
 
-            if (currentLoadedPoints !== filePrefix) {
-                for (let x = 0; x < 5; x++) {
-                    for (let y = 0; y < 5; y++) {
-                        const spObject = elements.strongpoints[x][y];
+            for (let x = 0; x < 5; x++) {
+                for (let y = 0; y < 5; y++) {
+                    const spObject = elements.strongpoints[x][y];
 
-                        spObject.visible = controls.checkStrongpoints.is(":checked") && $(".sp-toggle-" + x + y).hasClass("selected");
+                    spObject.visible = controls.checkStrongpoints.is(":checked") && $(".sp-toggle-" + x + y).hasClass("selected");
 
-                        if (!pointCutoutData.hasOwnProperty(filePrefix)) {
-                            continue;
-                        }
+                    if (!pointCutoutData.hasOwnProperty(filePrefix)) {
+                        continue;
+                    }
 
+                    if (currentLoadedPoints !== filePrefix) {
                         const pointData = pointCutoutData[filePrefix]['' + x + y];
                         if (pointData.visible) {
                             spObject.setSrc(pointData.dataUrl, internal.render);
@@ -494,17 +520,21 @@
                         }
                     }
                 }
+            }
 
+            if (currentLoadedPoints !== filePrefix) {
                 currentLoadedPoints = filePrefix;
             }
 
             controls.fabricCanvas.renderAll();
+            controls.exportCanvas.renderAll();
         },
 
         render: function () {
             console.log("render()");
 
             controls.fabricCanvas.renderAll();
+            controls.exportCanvas.renderAll();
         }
     }
 
