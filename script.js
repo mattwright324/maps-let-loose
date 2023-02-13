f3aefb8fddc29682a3a03b85b26a7f9856df8064
Matthew Wright <mattwright324@gmail.com>
2023-02-13 11:03:43 -0500

    remove unintended drag select mode after creating a polygon

--- script.js
+++ script.js
@@ -2375,7 +2375,6 @@ const mll = (function () {
 
                     canvas.add(line);
                     canvas.add(circle);
-                    canvas.selection = false;
                 },
                 generatePolygon: function (pointArray) {
                     var points = new Array();
@@ -2416,7 +2415,6 @@ const mll = (function () {
                     activeLine = null;
                     activeShape = null;
                     polygonMode = false;
-                    canvas.selection = true;
 
                     placed.push(polygon);
                     addAndOrder(polygon);
