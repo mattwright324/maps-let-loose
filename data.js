d1cdfe78ded894b2ae0ed4677976538a6408972f
Matthew Wright <mattwright324@gmail.com>
2023-05-05 12:05:57 -0400

    New maps fix sectors

--- data.js
+++ data.js
@@ -15,17 +15,17 @@ const POINT_COORDS = {
         [null, null, null, null, null]
     ],
     Driel: [
-        [null, null, null, null, null],
-        [null, null, null, null, null],
-        [null, null, null, null, null],
-        [null, null, null, null, null],
-        [null, null, null, null, null]
+        [null, [[0, 0, 0, 0]], [[0, 0, 0, 0]], [[0, 0, 0, 0]], null],
+        [null, [[0, 0, 0, 0]], [[0, 0, 0, 0]], [[0, 0, 0, 0]], null],
+        [null, [[0, 0, 0, 0]], [[0, 0, 0, 0]], [[0, 0, 0, 0]], null],
+        [null, [[0, 0, 0, 0]], [[0, 0, 0, 0]], [[0, 0, 0, 0]], null],
+        [null, [[0, 0, 0, 0]], [[0, 0, 0, 0]], [[0, 0, 0, 0]], null],
     ],
     ElAlamein: [
         [null, null, null, null, null],
-        [null, null, null, null, null],
-        [null, null, null, null, null],
-        [null, null, null, null, null],
+        [[[0, 0, 0, 0]], [[0, 0, 0, 0]], [[0, 0, 0, 0]], [[0, 0, 0, 0]], [[0, 0, 0, 0]]],
+        [[[0, 0, 0, 0]], [[0, 0, 0, 0]], [[0, 0, 0, 0]], [[0, 0, 0, 0]], [[0, 0, 0, 0]]],
+        [[[0, 0, 0, 0]], [[0, 0, 0, 0]], [[0, 0, 0, 0]], [[0, 0, 0, 0]], [[0, 0, 0, 0]]],
         [null, null, null, null, null]
     ],
     Foy: [
