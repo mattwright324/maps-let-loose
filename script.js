5ddae0c72b75b0d7a848ae5fe457ecf920009a1d
mattwright324 <mattwright324@gmail.com>
2025-08-31 10:05:18 -0400

    bigger handle sizes

--- script.js
+++ script.js
@@ -1013,7 +1013,7 @@ const mll = (function () {
         actionHandler: fabric.controlsUtils.rotationWithSnapping,
         actionName: 'rotate',
         render: renderRotateIcon,
-        cornerSize: 24,
+        cornerSize: 32,
         withConnection: true
     });
 
@@ -2957,7 +2957,7 @@ const mll = (function () {
                 cursorStyle: 'pointer',
                 mouseUpHandler: deleteObject,
                 render: renderIcon,
-                cornerSize: 24
+                cornerSize: 32
             });
 
             const dragSvg = "data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='16' height='16' style='stroke:white;stroke-width:1px;' class='bi bi-arrows-move' viewBox='0 0 16 16'%3E%3Cpath fill-rule='evenodd' d='M7.646.146a.5.5 0 0 1 .708 0l2 2a.5.5 0 0 1-.708.708L8.5 1.707V5.5a.5.5 0 0 1-1 0V1.707L6.354 2.854a.5.5 0 1 1-.708-.708l2-2zM8 10a.5.5 0 0 1 .5.5v3.793l1.146-1.147a.5.5 0 0 1 .708.708l-2 2a.5.5 0 0 1-.708 0l-2-2a.5.5 0 0 1 .708-.708L7.5 14.293V10.5A.5.5 0 0 1 8 10zM.146 8.354a.5.5 0 0 1 0-.708l2-2a.5.5 0 1 1 .708.708L1.707 7.5H5.5a.5.5 0 0 1 0 1H1.707l1.147 1.146a.5.5 0 0 1-.708.708l-2-2zM10 8a.5.5 0 0 1 .5-.5h3.793l-1.147-1.146a.5.5 0 0 1 .708-.708l2 2a.5.5 0 0 1 0 .708l-2 2a.5.5 0 0 1-.708-.708L14.293 8.5H10.5A.5.5 0 0 1 10 8z'/%3E%3C/svg%3E"
@@ -2982,7 +2982,7 @@ const mll = (function () {
                 actionName: 'drag',
                 cursorStyle: 'pointer',
                 render: renderIconDrag,
-                cornerSize: 24
+                cornerSize: 32
             });
 
             function renderIcon(ctx, left, top, styleOverride, fabricObject) {
